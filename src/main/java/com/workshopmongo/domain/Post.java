87b48351a8b5ed6118c54fa07bc6149e7409b929
eduardo <eduardofs_2016@outlook.com>
2022-05-05 20:30:59 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/workshopmongo/domain/Post.java
+++ src/main/java/com/workshopmongo/domain/Post.java
@@ -1,29 +1,36 @@
 package com.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.workshopmongo.dto.AuthorDTO;
+import com.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
 	private String title;
 	private String body;
 	private AuthorDTO author;
-	
-	public Post() {}
 
-	public Post(String id, Date date, String title, String body, AuthorDTO author) {;
+	private List<CommentDTO> comment = new ArrayList<>();
+
+	public Post() {
+	}
+
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
+		;
 		this.id = id;
 		this.date = date;
 		this.title = title;
@@ -62,13 +69,21 @@ public class Post implements Serializable {
 	public void setBody(String body) {
 		this.body = body;
 	}
-	
+
 	public AuthorDTO getAuthor() {
 		return author;
 	}
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComment() {
+		return comment;
+	}
+
+	public void setComment(List<CommentDTO> comment) {
+		this.comment = comment;
 	}
 
 	@Override
