44da5c287ff36fc3bf2e866751f91e6247566be2
eduardo <eduardofs_2016@outlook.com>
2022-05-05 17:27:12 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/workshopmongo/services/exception/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.workshopmongo.services.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.workshopmongo.resources.exception.StandardError;
+
+@ControllerAdvice // diz que essa classe é capaz de tratar erros na aplicação
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err); 
+		
+	}
+	
+
+}
